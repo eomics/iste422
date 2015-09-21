7cf904e9d5f4fd068cdec0b5dc3bc855b1bf3080
Eduard Martinescu <eomics@rit.edu>
2015-09-21 18:29:08 -0400

    make it more realistice

--- src/main/java/oata/HelloWorld.java
+++ src/main/java/oata/HelloWorld.java
@@ -2,14 +2,22 @@ package oata;
 
 // Import log4j classes.
 import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.apache.logging.log4j.LogManager;
 
 public class HelloWorld {
   private static final Logger LOG = LogManager.getLogger(HelloWorld.class);
 
   public static void main(String[] args) {
+    String w = null;
     try {
-      causeError();
+      w = causeError2();
     }
     catch (Exception e) {
        System.err.println("There was a problem!");
@@ -20,19 +28,45 @@ public class HelloWorld {
     catch (Exception e) {
        System.err.println("There was a problem!\nPlease fix the code");
     }
-    LOG.info("Hello World");
+    LOG.info(w);
   }
 
-  public static void causeError() {
-    throw new RuntimeException("This exception is on purpose");
-  }
-  public static void causeError2() {
+  public static String readFile(InputStream fs) throws IOException {
+    BufferedReader br = null;
+    StringBuilder sb = new StringBuilder();
+
+    String line;
     try {
-      causeError();
+
+      br = new BufferedReader(new InputStreamReader(fs));
+      while ((line = br.readLine()) != null) {
+        sb.append(line);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+      throw e;
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
     }
-    catch (RuntimeException e) {
+
+    return sb.toString();
+  }
+  public static String causeError2() throws IOException {
+    StringBuilder sb = new StringBuilder();
+    try (FileInputStream fs = (FileInputStream) HelloWorld.class.getClassLoader().getResourceAsStream("myfile")){
+      sb.append(readFile(fs));
+    }
+    catch (IOException e) {
       System.err.println("I caught a problem");
       throw e;
     }
+    return sb.toString();
   }
 }
